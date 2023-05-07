33adcf1922e0ca9d9ac5d80b973034e60996b8f9
Kazuya08 <kazuyathiago@hotmail.com>
2023-05-07 14:05:09 -0300

    feat: add sql add message

--- sql/queries/query.sql
+++ sql/queries/query.sql
@@ -4,5 +4,8 @@ INSERT INTO chats
     (id, user_id, initial_message_id, status, token_usage, model, model_max_tokens,temperature, top_p, n, stop, max_tokens, presence_penalty, frequency_penalty, created_at, updated_at)
     VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);
 
+-- name: AddMessage :exec
+INSERT INTO messages (id, chat_id, role, content, tokens, model, erased, order_msg, created_at) VALUES (?,?,?,?,?,?,?,?,?);
+
 -- name: FindChatById :one
 SELECT * FROM chats WHERE id = ?;
